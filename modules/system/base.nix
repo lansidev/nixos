f0edb569a0ce841b30b46b1daa81ac88ed4544b7
Simon Lansing <55317770+simlans@users.noreply.github.com>
2026-05-03 20:50:02 +0200

    Switch system UI locale to en_US.UTF-8 (TTY keymap stays DE)

--- modules/system/base.nix
+++ modules/system/base.nix
@@ -2,23 +2,13 @@
 {
   time.timeZone = "Europe/Berlin";
 
-  i18n.defaultLocale = "de_DE.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
+  i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = [
     "en_US.UTF-8/UTF-8"
     "de_DE.UTF-8/UTF-8"
   ];
 
+  # TTY keyboard stays DE-ISO; UI language is English.
   console.keyMap = "de";
 
   nix.settings = {
